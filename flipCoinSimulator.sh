a44cb6918d36e2fb159449f95cea12fc0268856d
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-03 00:31:24 +0530

    Added: Won 21 times. win or tie. if win them who won and by how much

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,22 +1,31 @@
-#!/bin/bash -x
-
-totalcount=100
+#!/bin/bash
 
 #variables
 headcount=0
 tailcount=0
 
-while [ $totalcount -gt 0 ]
+#use while loop
+while [ $tailcount -lt 21  ] && [ $headcount -lt 21 ]
 do
-if [ $((RANDOM%2)) -eq 1 ]
-then
-# let is use for evaluate
-let headcount++
-else
-let tailcount++
-fi
-let totalcount--
+
+	if [ $((RANDOM%2)) -eq 1 ];then
+	let headcount++
+	else
+	let tailcount++
+	fi
 done
 
-echo "tail wins $tailcount times"
-echo "head wins $headcount times"
+echo "head score::$headcount"
+echo "tail score::$tailcount"
+
+if [ $headcount -gt $tailcount ];then
+echo "Head wins total score is: $headcount"
+echo "Head wins by $(($headcount-$tailcount)) points from Tail"
+
+elif [ $headcount -lt $tailcount ];then
+echo "Tail wins total score is: $tailcount"
+echo "Tail wins by $(($tailcount-$headcount)) point from Head"
+
+else
+echo "Its a tie match:: Head score:$headcount :: Tail score:$tailcount"
+fi
