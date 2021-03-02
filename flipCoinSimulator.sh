56713ff8b56d07761e43ee2dcb4b7d757ace9fbe
Deva88 <devendra.raj.sdm@gmail.com>
2021-03-02 23:08:44 +0530

    Added: As a simulator start with Fliping Coin to Display Head or Tails

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,13 @@
-# welcom to FlipCoin Simulator Problem
+#!/bin/bash -x
+
+#CONSTANT
+HEAD=0
+
+#TO PRINT THE HEAD AND TAIL
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
